c6b594a6135ed58665ef3025c5e40bbe5a57252b
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-04-19 14:29:59 +0200

    Add scenario: convert 1 to I

---
+++ src/RomanNumerals.hs
@@ -0,0 +1,7 @@
+module RomanNumerals where
+
+
+
+convertToRomanNumeral :: Int -> String
+convertToRomanNumeral 1 = "I"
+convertToRomanNumeral _ = error "Not implemented"
--- test/RomanNumeralsSpec.hs
+++ test/RomanNumeralsSpec.hs
@@ -1,11 +1,14 @@
 module RomanNumeralsSpec where
 
+import RomanNumerals
 import Test.Hspec
+import Control.Monad (forM_)
 
 spec :: Spec
 spec = do
 
   describe "Number to Roman Numeral" $ do
 
-    it "should work" $ do
-      1 `shouldBe` 1
+    forM_ [ (1,"I"), ] $ \(number, romanNumeral) ->
+      it ("should convert " ++ (show number) ++ " to "++ (show romanNumeral)) $ do
+        convertToRomanNumeral number `shouldBe` romanNumeral
