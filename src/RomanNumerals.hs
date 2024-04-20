44e5f9e3d50429a1c0080364ea0c844eb36ee952
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-04-20 07:32:37 +0200

    Add scenario: convert 2 and 3

--- src/RomanNumerals.hs
+++ src/RomanNumerals.hs
@@ -4,4 +4,6 @@ module RomanNumerals where
 
 convertToRomanNumeral :: Int -> String
 convertToRomanNumeral 1 = "I"
+convertToRomanNumeral 2 = "II"
+convertToRomanNumeral 3 = "III"
 convertToRomanNumeral _ = error "Not implemented"
--- test/RomanNumeralsSpec.hs
+++ test/RomanNumeralsSpec.hs
@@ -9,6 +9,10 @@ spec = do
 
   describe "Number to Roman Numeral" $ do
 
-    forM_ [ (1,"I"), ] $ \(number, romanNumeral) ->
+    forM_ [
+            (1,"I"),
+            (2, "II"),
+            (3, "III")
+          ] $ \(number, romanNumeral) ->
       it ("should convert " ++ (show number) ++ " to "++ (show romanNumeral)) $ do
         convertToRomanNumeral number `shouldBe` romanNumeral
