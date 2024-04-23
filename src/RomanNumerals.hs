2e5a14ec980132a92d748e720f52d5efde9c28d8
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-04-23 08:24:59 +0200

    Small refactor: Preparing the code for a big refactor

--- src/RomanNumerals.hs
+++ src/RomanNumerals.hs
@@ -7,11 +7,11 @@ toRoman' :: Int -> String -> String
 toRoman' num acc
   | num == 0 = acc
   | num < 4 = toRoman' (num - 1) (acc ++ "I")
-  | num == 4 = "IV"
+  | num == 4 = toRoman' (num - 4) (acc ++ "IV")
   | num < 9 = toRoman' (num - 5) (acc ++ "V")
-  | num == 9 = "IX"
+  | num == 9 = toRoman' (num - 9) (acc ++ "IX")
   | num < 40 = toRoman' (num - 10) (acc ++ "X")
   | num < 50 && num >= 40 = toRoman' (num - 40) (acc ++ "XL")
-  | num == 50 = "L"
+  | num == 50 = toRoman' (num - 50) (acc ++ "L")
   | otherwise = error "Not implemented"
 
