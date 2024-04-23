ccee34eb901b9eb068227453020db128b7a93702
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-04-23 08:04:34 +0200

    Add scenario: convert nine

--- src/RomanNumerals.hs
+++ src/RomanNumerals.hs
@@ -8,6 +8,7 @@ toRoman' num acc
   | num == 0 = acc
   | num < 4 = toRoman' (num - 1) (acc ++ "I")
   | num == 4 = "IV"
-  | num <= 9 = toRoman' (num - 5) (acc ++ "V")
+  | num < 9 = toRoman' (num - 5) (acc ++ "V")
+  | num == 9 = "IX"
   | otherwise = error "Not implemented"
 
