e8be3dd085d08bc6d9c8ff993715936b1b1099da
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-04-24 06:18:45 +0200

    Add scenarios: Add the rest of test cases, up to 3XXX

--- src/RomanNumerals.hs
+++ src/RomanNumerals.hs
@@ -1,4 +1,4 @@
-module RomanNumerals where
+module RomanNumerals (toRoman) where
 
 import qualified Data.List as List
 
@@ -6,15 +6,16 @@ toRoman :: Int -> String
 toRoman num = toRoman' num ""
 
 mappings :: [(Int, String)]
-mappings = [(50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")]
+mappings = [
+  (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), (90, "XC"),
+  (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
+  ]
 
 toRoman' :: Int -> String -> String
-toRoman' num acc = if num == 0
-                   then acc
-                   else toRoman' (num - value) (acc ++ roman)
-                 where mapping = findMappingFor num
-                       value = fst mapping
-                       roman = snd mapping
+toRoman' num acc = if num == 0 then acc else toRoman' (num - value) (acc ++ roman)
+  where mapping = findMappingFor num
+        value = fst mapping
+        roman = snd mapping
 
 findMappingFor :: Int -> (Int, String)
 findMappingFor num = List.head $ List.filter (\(n, _) -> n <= num) mappings
--- test/RomanNumeralsSpec.hs
+++ test/RomanNumeralsSpec.hs
@@ -9,22 +9,10 @@ spec = do
 
   describe "Number to Roman Numeral" $ do
 
-    forM_ [
-            (1,"I"),
-            (2, "II"),
-            (3, "III"),
-            (4, "IV"),
-            (5, "V"),
-            (6, "VI"),
-            (7, "VII"),
-            (8, "VIII"),
-            (9, "IX"),
-            (10, "X"),
-            (15, "XV"),
-            (27, "XXVII"),
-            (40, "XL"),
-            (46, "XLVI"),
-            (50, "L")
+    forM_ [ (1,"I"), (2, "II"), (3, "III"), (4, "IV"), (5, "V"), (6, "VI"), (7, "VII"),
+            (8, "VIII"), (9, "IX"), (10, "X"), (15, "XV"), (27, "XXVII"), (40, "XL"),
+            (46, "XLVI"), (50, "L"), (59, "LIX"), (93, "XCIII"), (100, "C"), (478, "CDLXXVIII"),
+            (666, "DCLXVI"), (500, "D"), (999, "CMXCIX"), (1000, "M"), (3888, "MMMDCCCLXXXVIII")
           ] $ \(number, romanNumeral) ->
       it ("should convert " ++ (show number) ++ " to "++ (show romanNumeral)) $ do
         toRoman number `shouldBe` romanNumeral
